26ddf48baec0ae82ab29c5c96fc1b34a39f5f96e
joperico <estevezcarmona15@gmail.com>
2026-06-16 18:33:50 +0200

    Update miapp.py

--- fuentes/miapp.py
+++ fuentes/miapp.py
@@ -14,7 +14,7 @@ engine = create_engine(DATABASE_URL)
 
 @app.get("/status")
 def status():
-    return {"message": "FastAPI Apellidos-Nombre v.3.0"}
+    return {"message": "FastAPI Apellidos-Nombre v.4.0"}
 
 
 @app.get("/check")
